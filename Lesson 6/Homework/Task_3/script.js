7a54010239f0a152bb2dbbdfabee0acad0a78ce5
DanilKarasev <88264176+DanilKarasev@users.noreply.github.com>
2021-09-23 19:11:23 +1000

    Дополнил слайдер миниатюрами. Не могу сообразить как добавить событие по клику на миниатюру, чтобы слайдер при этом менялся.

--- Lesson 6/Homework/Task_3/script.js
+++ Lesson 6/Homework/Task_3/script.js
@@ -2,24 +2,30 @@ var prev = document.querySelector(".carousel-control-prev");
 var next = document.querySelector(".carousel-control-next");//Получили кнопки
 
 var images = document.querySelectorAll(".carousel-item");//получили массив блоков с изображениями
+var miniImages = document.querySelectorAll(".mini_image");//Миниатюры изображений
 var i = 2; //Решил начинать со второго элемента, можно менять
 
 images[i].classList.add('active'); //можно было просто добавлять атрибут, но решил через класс, как изначально было в бутстрапе
+miniImages[i].classList.add('mini_image_active');
 
-prev.onclick = function(){
+prev.onclick = function () {
     images[i].classList.remove('active');
+    miniImages[i].classList.remove('mini_image_active');
     i -= 1;
-    if(i < 0){
+    if (i < 0) {
         i = images.length - 1;
     }
     images[i].classList.add('active');
+    miniImages[i].classList.add('mini_image_active');
 }
-next.onclick = function(){
+
+next.onclick = function () {
     images[i].classList.remove('active');
+    miniImages[i].classList.remove('mini_image_active');
     i += 1;
-    if(i == images.length){
+    if (i == images.length) {
         i = 0;
     }
     images[i].classList.add('active');
+    miniImages[i].classList.add('mini_image_active');
 }
-
